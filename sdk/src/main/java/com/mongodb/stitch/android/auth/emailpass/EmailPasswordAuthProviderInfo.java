7a5d08b6048d618f6112e8fd788b09af031e6264
Jay Flax <jsflax@Jays-MacBook-Pro.local>
2017-06-19 14:46:53 -0500

    Quick fix for EmailPasswordAuthProviderInfo jackson properties ignoring

--- sdk/src/main/java/com/mongodb/stitch/android/auth/emailpass/EmailPasswordAuthProviderInfo.java
+++ sdk/src/main/java/com/mongodb/stitch/android/auth/emailpass/EmailPasswordAuthProviderInfo.java
@@ -1,9 +1,12 @@
 package com.mongodb.stitch.android.auth.emailpass;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * EmailPasswordAuthProviderInfo contains information needed to create
  * a {@link EmailPasswordAuthProvider}.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EmailPasswordAuthProviderInfo {
     public static final String FQ_NAME = "local/userpass";
 }
