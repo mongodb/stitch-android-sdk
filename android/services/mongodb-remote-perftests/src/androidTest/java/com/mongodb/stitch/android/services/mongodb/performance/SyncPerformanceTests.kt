436ef609e60073a29205babc7762cd187eb446b2
Tyler Kaye <tyler.kaye@mongodb.com>
2019-04-16 11:14:44 -0400

    Revert "Revert "STITCH-2736: adds various improvements to performance testing harness" (#139)"
    This reverts commit 32d1fc9fb11a79f626ed3ae6827eb151c4b1e125.

--- android/services/mongodb-remote-perftests/src/androidTest/java/com/mongodb/stitch/android/services/mongodb/performance/SyncPerformanceTests.kt
+++ android/services/mongodb-remote-perftests/src/androidTest/java/com/mongodb/stitch/android/services/mongodb/performance/SyncPerformanceTests.kt
@@ -1,22 +1,22 @@
 package com.mongodb.stitch.android.services.mongodb.performance
 
 import android.support.test.runner.AndroidJUnit4
+import android.util.Log
 
 import com.google.android.gms.tasks.Tasks
+import com.mongodb.stitch.core.services.mongodb.remote.ExceptionListener
+import com.mongodb.stitch.core.services.mongodb.remote.sync.DefaultSyncConflictResolvers
 
-import org.bson.types.Binary
 import org.bson.Document
 import org.bson.types.ObjectId
-import org.junit.After
 
-import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+val testHarness = SyncPerformanceIntTestsHarness()
+
 @RunWith(AndroidJUnit4::class)
 class SyncPerformanceTests {
-    private val outputToStitch = false
 
     companion object {
         private val runId by lazy {
@@ -24,91 +24,86 @@ class SyncPerformanceTests {
         }
     }
 
-    private var testHarness = SyncPerformanceIntTestsHarness()
-
-    @Before
-    fun setup() {
-        this.testHarness.setup()
-    }
-
-    @After
-    fun teardown() {
-        this.testHarness.teardown()
-    }
-
     // Tests for L2R-only scenarios
-
     @Test
     fun testL2ROnlyInitialSync() {
-        SyncL2ROnlyPerformanceTestDefinitions.testInitialSync(this.testHarness, runId)
+        SyncL2ROnlyPerformanceTestDefinitions.testInitialSync(testHarness, runId)
     }
 
     @Test
     fun testL2ROnlyDisconnectReconnect() {
-        SyncL2ROnlyPerformanceTestDefinitions.testDisconnectReconnect(this.testHarness, runId)
+        SyncL2ROnlyPerformanceTestDefinitions.testDisconnectReconnect(testHarness, runId)
     }
 
     @Test
     fun testL2ROnlySyncPass() {
-        SyncL2ROnlyPerformanceTestDefinitions.testSyncPass(this.testHarness, runId)
+        SyncL2ROnlyPerformanceTestDefinitions.testSyncPass(testHarness, runId)
     }
 
     // Placeholder tests until the whole performance testing suite is in place
-
     @Test
-    fun testInitialSync() {
-        val testParams = TestParams(
-            runId = runId,
-            testName = "testInitialSync",
-            dataProbeGranularityMs = 400L,
-            docSizes = intArrayOf(50),
-            numDocs = intArrayOf(10),
-            numIters = SyncPerformanceTestUtils.getConfiguredIters(),
-            numOutliersEachSide = 0,
-            outputToStitch = outputToStitch,
-            stitchHostName = SyncPerformanceTestUtils.getConfiguredStitchHostname()
-        )
+    fun sampleTest1() {
+        var documents1 = arrayListOf<Document>().toList()
+        var documents2 = arrayListOf<Document>().toList()
 
-        testHarness.runPerformanceTestWithParams(testParams, { ctx, docSize, numDocs ->
-            val documents = getDocuments(numDocs, docSize)
-            assertEquals(0L, Tasks.await(ctx.testColl.count()))
-            Tasks.await(ctx.testColl.insertMany(documents))
-            assertEquals(numDocs.toLong(), Tasks.await(ctx.testColl.count()))
+        testHarness.runPerformanceTestWithParams("sampleTest1", runId,
+            testDefinition = { ctx, numDocs, docSize ->
+                testHarness.logMessage(String.format("Running %s with %d docs of size %d",
+                    "initialSync", docSize, numDocs))
+
+                assertEqualsThrows(0L, Tasks.await(ctx.testColl.count()))
+                Tasks.await(ctx.testColl.insertMany(documents1))
+                assertEqualsThrows(numDocs.toLong(), Tasks.await(ctx.testColl.count()))
+
+                Tasks.await(ctx.testColl.sync().insertMany(documents2))
+                doSyncPass(ctx)
+            }, beforeEach = { ctx, numDocs, docSize ->
+                testHarness.logMessage(String.format("Custom beforeEach for %d docs of size %d",
+                    docSize, numDocs))
+
+                documents1 = SyncPerformanceTestUtils.generateDocuments(docSize, numDocs)
+                documents2 = SyncPerformanceTestUtils.generateDocuments(docSize, numDocs)
+
+                Tasks.await(ctx.testColl.sync().configure(
+                    DefaultSyncConflictResolvers.remoteWins(),
+                    null,
+                    ExceptionListener { id, ex ->
+                        testHarness.logMessage("unexpected sync error with id " +
+                            "$id: ${ex.localizedMessage}")
+                        error(ex)
+                    }
+                ))
+        }, afterEach = { ctx, numDocs, docSize ->
+                testHarness.logMessage(String.format("Custom afterEach for %d docs of size %d",
+                    docSize, numDocs))
+                assertEqualsThrows(numDocs, Tasks.await(ctx.testColl.sync().count()).toInt())
+                assertEqualsThrows(numDocs, Tasks.await(ctx.testColl.sync().syncedIds).size)
+                assertEqualsThrows((numDocs * 2).toLong(), Tasks.await(ctx.testColl.count()))
         })
     }
 
-    @Test
-    fun testDisconnectReconnect() {
-        val testParams = TestParams(
-            runId = runId,
-            testName = "testDisconnectReconnect",
-            dataProbeGranularityMs = 400L,
-            docSizes = intArrayOf(30, 60),
-            numDocs = intArrayOf(10),
-            numIters = SyncPerformanceTestUtils.getConfiguredIters(),
-            numOutliersEachSide = 0,
-            outputToStitch = outputToStitch,
-            stitchHostName = SyncPerformanceTestUtils.getConfiguredStitchHostname()
-        )
-
-        testHarness.runPerformanceTestWithParams(testParams, { ctx, docSize, numDocs ->
-            val documents = getDocuments(numDocs, docSize)
-            assertEquals(Tasks.await(ctx.testColl.count()), 0L)
-            Tasks.await(ctx.testColl.insertMany(documents))
-            assertEquals(Tasks.await(ctx.testColl.count()), numDocs.toLong())
-        })
+    fun <T> assertEqualsThrows(expected: T, actual: T, message: String = "") {
+        if (expected == actual) {
+            return
+        }
+        throw Exception(String.format("Expected: %s but found %s: %s", expected, actual, message))
     }
 
-    private fun getDocuments(
-        numberOfDocs: Int,
-        sizeOfDocsInBytes: Int
-    ): List<Document>? {
-        val array: List<Byte> = (0 until sizeOfDocsInBytes).map { 0.toByte() }
-        return (0 until numberOfDocs).map {
-            Document(mapOf(
-                    "_id" to ObjectId(),
-                    "bin" to Binary(array.toByteArray())
-            ))
+    private fun doSyncPass(ctx: SyncPerformanceTestContext) {
+        if (ctx.testNetworkMonitor.connectedState) {
+            var counter = 0
+            while (!ctx.testDataSynchronizer.areAllStreamsOpen()) {
+                Thread.sleep(5)
+
+                // if this hangs longer than 30 seconds, throw an error
+                counter += 1
+                if (counter > 500) {
+                    Log.e("perfTests", "stream never opened after reconnect")
+                    error("stream never opened after reconnect")
+                }
+            }
+
+            ctx.testDataSynchronizer.doSyncPass()
         }
     }
 }
