c580e2bbc796677a056dc676d631c676bf7d4ec9
Jason Flax <jsflax@gmail.com>
2019-10-17 11:42:38 +0100

    Add javadoc/mongodb template

--- core/sdk/src/main/java/com/mongodb/stitch/core/auth/providers/function/FunctionCredential.java
+++ core/sdk/src/main/java/com/mongodb/stitch/core/auth/providers/function/FunctionCredential.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mongodb.stitch.core.auth.providers.function;
 
 import com.mongodb.stitch.core.auth.ProviderCapabilities;
@@ -5,6 +21,10 @@ import com.mongodb.stitch.core.auth.StitchCredential;
 
 import org.bson.Document;
 
+
+/**
+ * The credential used for custom function log ins.
+ */
 public class FunctionCredential implements StitchCredential {
   private final String providerName;
   private final Document payload;
